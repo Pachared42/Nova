d3d37ce5fffb5ed1be0421f46f22d7437262cb02
Axm.pachara <pachared.amr12@gmail.com>
2025-12-18 17:15:07 +0700

    ปรับแต่ง UI ให้เข้ากับขนาดหน้าจอและก็เพิ่มข้อมูลการแสดงต่างๆ

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,8 +7,8 @@ import { HeroConstants } from '../constants/constants'
 
 function HeroSection() {
     return (
-        <div className="relative w-full overflow-visible items-center justify-center px-10 py-10 md:px-26 md:py-26">
-            <div className="absolute inset-0 opacity-50 z-0 overflow-hidden">
+        <div className="relative w-full overflow-visible items-center justify-center py-10 md:py-20 2xl:px-26 2xl:py-26">
+            <div className="absolute inset-0 opacity-40 z-0 overflow-hidden">
                 <ColorBends
                     className="pointer-events-auto"
                     colors={["#8A2CFF", "#7F00FF", "#3A86FF", "#C87BFF"]}
@@ -29,12 +29,12 @@ function HeroSection() {
                 />
             </div>
 
-            <div className="relative z-10 mt-40 mb-25 flex flex-col items-center justify-center space-y-3">
+            <div className="relative z-10 mt-20 mb-25 2xl:mt-40 2xl:mb-25 flex flex-col items-center justify-center space-y-3">
                 <div className="flex items-center justify-center space-x-5 ">
-                    <h1 className="tracking-tighter text-xs sm:text-sm md:text-7xl font-bold"> {HeroConstants.mainTitle} </h1>
+                    <h1 className="tracking-tighter text-xs sm:text-sm md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-7xl font-bold"> {HeroConstants.mainTitle} </h1>
                     <RotatingText
                         texts={['Build', 'Test', 'Deploy']}
-                        mainClassName="flex item-center justify-center leading-none px-2 sm:px-3 md:px-4 border-b border-white/30 backdrop-blur-xl text-white text-xs sm:text-sm md:text-7xl font-bold overflow-hidden py-0.5 sm:py-1 md:py-2 rounded-3xl shadow-xl"
+                        mainClassName="flex item-center justify-center leading-none px-2 sm:px-3 md:px-4 border-b border-white/30 backdrop-blur-xl text-white text-xs sm:text-sm md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-7xl font-bold overflow-hidden py-0.5 sm:py-1 md:py-2 rounded-3xl shadow-xl"
                         staggerFrom={"first"}
                         initial={{ y: "100%", opacity: 100 }}
                         animate={{ y: 0 }}
@@ -50,9 +50,9 @@ function HeroSection() {
                         rotationInterval={2000}
                     />
                 </div>
-                <h1 className="tracking-tighter text-xs sm:text-sm md:text-5xl font-bold"> {HeroConstants.subTitle} </h1>
-                <p className="tracking-tight text-xs sm:text-sm md:text-xl text-zinc-400 mt-20"> {HeroConstants.description} </p>
-                <button className="mt-6 px-6 py-4 text-white tracking-tight text-xs sm:text-sm md:text-base font-medium cursor-pointer rounded-2xl border border-white/30 backdrop-blur-xl hover:bg-gray-500/15 transition-all duration-300 shadow-xl">
+                <h1 className="tracking-tighter text-xs sm:text-sm md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-6xl font-bold"> {HeroConstants.subTitle} </h1>
+                <p className="tracking-tight text-xs sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-xl text-zinc-400 mt-20"> {HeroConstants.description} </p>
+                <button className="mt-6 px-6 py-4 text-white tracking-tight text-xs sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-xl font-medium cursor-pointer rounded-2xl border border-white/30 backdrop-blur-xl hover:bg-gray-500/15 transition-all duration-300 shadow-xl">
                     {HeroConstants.buttonTextNewProject}
                 </button>
             </div>
--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -49,3 +49,9 @@ export const techLogosConstants = [
     href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
   },
 ];
+
+export const LoginText ={
+  welcomeMessage: "ยินดีต้อนรับสู่ Nova",
+  instructionMessage: "Deploy ง่ายด้วย GitHub Repository ",
+  githubButtonText: "ดำเนินการต่อด้วย Github",
+};
